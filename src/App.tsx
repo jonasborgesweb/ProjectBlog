5001799ca14e3abd9a4fdc63d85b56050eded39d
Gabriel Machado <gadu.machado@gmail.com>
2020-06-26 11:57:26 -0300

    fixed scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
@@ -7,13 +7,15 @@ import Routes from './routes/';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './routes/scroll'
 
 const App: React.FC = () => (
   <BrowserRouter>
+    <ScrollToTop />
     <Header />
-      <Routes />
+    <Routes />
     <Footer />
-    <GlobalStyle/>
+    <GlobalStyle />
   </BrowserRouter>
 )
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import SinglePosts from '../pages/SinglePosts';
 import SingleUsers from '../pages/SingleUsers';
 
-const Routes:React.FC = () => (
+const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Home}/>
-    <Route path='/post/:id' component={SinglePosts}/>
+    <Route path="/" exact component={Home} />
+    <Route path='/post/:id' component={SinglePosts} />
     <Route path='/users/:id' component={SingleUsers} />
   </Switch>
 )
